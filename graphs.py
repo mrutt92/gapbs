dd5eb1a85f1fa18807d06e01862307da50f41f38
Max Ruttenberg <mrutt@cs.washington.edu>
2022-06-14 08:52:54 -0700

    updates list of generated graphs

--- graphs.py
+++ graphs.py
@@ -1,9 +1,10 @@
 def graph(gtype, gscale, gavgdegree):
     ofile.write("graphs += $(call graph,{},{},{}).al\n".format(gtype,gscale,gavgdegree))
+    ofile.write("graphs += $(call graph,{},{},{}).el\n".format(gtype,gscale,gavgdegree))
 
 gtypes = ['u','g']
 #gscales = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-gscales = [10, 11]
+gscales = [10, 11, 16, 20]
 gavgdegrees = [8, 16, 32]
 
 with open('graphs.mk','w') as ofile:
